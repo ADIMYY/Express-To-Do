c641f2e3a2a7228ec28530cd70fb4eb60219cf8b
Abdelrahman Ali Elsayed <146658311+ADIMYY@users.noreply.github.com>
2024-09-18 17:19:34 +0300

    Update task.js

--- models/task.js
+++ models/task.js
@@ -2,17 +2,9 @@ const mongoose = require('mongoose');
 
 
 const taskSchema = new mongoose.Schema({
-    title: {
-        type: 'string',
-        required: [true, 'task must have a title']
-    }, 
-    description: {
-        type: 'string'
-    }, 
-    completed: {
-        type: Boolean, 
-        default: false
-    }
+    title: { type: 'string', required: [true, 'task must have a title']}, 
+    description: { type: 'string' }, 
+    completed: { type: Boolean, default: false }
 });
 
 const Task = mongoose.model('Task', taskSchema);
